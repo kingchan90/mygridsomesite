11139040c55867a233531adc52362a6be5ebc424
quan.tran <quan.tran@it-consultis.com>
2019-09-04 10:48:44 +0700

    Updated the admin config.

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,17 +1,23 @@
 module.exports = {
-	siteName: 'Gridsome Starter NetlifyCMS',
-	siteUrl: 'https://www.gridsome.org',
-	siteDescription: 'Gridsome is a blazing-fast static site generator...',
-	titleTemplate: `%s - Gridsome`,
-	
-	plugins: [
-		{
-			use: '@gridsome/source-filesystem',
-			options: {
-				path: 'blog/*.md',
-				typeName: 'BlogPost',
-				route: '/blog/:slug'
-			}
-		}
-	]
-}
+  siteName: "Gridsome Starter NetlifyCMS",
+  siteUrl: "https://www.gridsome.org",
+  siteDescription: "Gridsome is a blazing-fast static site generator...",
+  titleTemplate: `%s - Gridsome`,
+
+  plugins: [
+    {
+      use: "@gridsome/source-filesystem",
+      options: {
+        path: "blog/*.md",
+        typeName: "BlogPost",
+        route: "/blog/:slug"
+      }
+    },
+    {
+      use: `gridsome-plugin-netlify-cms`,
+      options: {
+        publicPath: `/admin`
+      }
+    }
+  ]
+};
